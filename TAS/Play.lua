1fce5b5b91540ebf32734914ab0cde7da29d9725
interpreterK <80614657+interpreterK@users.noreply.github.com>
2022-01-09 15:04:24 -0500

    Update Play.lua

--- TAS/Play.lua
+++ TAS/Play.lua
@@ -4,50 +4,61 @@ Play.__metatable = nil
 
 local Twait = task.wait
 
-local function RootPlay(self, Root, i, Callback)
-    while true do
-        local M = (Root.Position - self.MoveData[i]).Magnitude
-        if math.floor(M) <= self.MinMagnitude then
-            break
-        else
-            Callback()
-        end
-        Twait()
-    end
+local function TAS_Move(self, Attr, i, Callback)
+	local Root = Attr[1]
+	local Humanoid = Attr[2]
+	
+	while true do
+		local Move = self.MoveData[i]
+		local M = (Root.Position - Move[2]).Magnitude
+		if math.floor(M) <= self.MinMagnitude then
+			if Move[1] ~= nil then
+				-- Custom actions wooo
+				if Move[1] == 'Jump' then
+					Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+					Humanoid.Jump = true
+				end
+			end
+			break
+		else
+			Callback()
+		end
+		Twait()
+	end
 end
 
 function Play : PlayAttempt(Attr)
-    local Start = tick()
+	local Start = tick()
 
-    local Root = Attr[1]
-    local Humanoid = Attr[2]
-    local MoveData = self.MoveData
-    local Freezing = self.Freezing
-    local PauseTime = self.PauseTime
+	local Root = Attr[1]
+	local Humanoid = Attr[2]
+	local MoveData = self.MoveData
+	local Freezing = self.Freezing
+	local PauseTime = self.PauseTime
 
-    if self.UseRawMovement then
-        if Freezing then
-            Root.Anchored = true
-        end
-        for i = 1, #MoveData do
-            RootPlay(self, Root, i, function()
-                for i2 = 1, 10 do
-                    Root.Position = Root.Position:Lerp(MoveData[i], i2/10)
-                    Twait(PauseTime)
-                end
-            end)
-        end
-        if Freezing then
-            Root.Anchored = false
-        end
-    else
-        for i = 1, #MoveData do
-            RootPlay(self, Root, i, function()
-                Humanoid:MoveTo(MoveData[i])
-            end)
-        end
-    end
-    print ("Run attempt completed. Time: " .. (tick() - Start))
+	if self.UseRawMovement then
+		if Freezing then
+			Root.Anchored = true
+		end
+		for i = 1, #MoveData do
+			TAS_Move(self, Attr, i, function()
+				for i2 = 1, 10 do
+					Root.Position = Root.Position:Lerp(MoveData[i][2], i2/10)
+					Twait(PauseTime)
+				end
+			end)
+		end
+		if Freezing then
+			Root.Anchored = false
+		end
+	else
+		for i = 1, #MoveData do
+			TAS_Move(self, Attr, i, function()
+				Humanoid:MoveTo(MoveData[i][2])
+			end)
+		end
+	end
+	print ("Run attempt completed. Time: " .. (tick() - Start))
 end
 
 return Play
